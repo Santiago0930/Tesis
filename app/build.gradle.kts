849d196f17420b5108673fe572128da4c1616a5f
Sebootas <dupersebas@gmail.com>
2025-05-17 21:53:02 -0500

    Merge 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 35 // Or your target SDK
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
